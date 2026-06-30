f5b02e1614915847ab6b5736b111e95214d60cbc
softwaremuregro <softwaremuregro@gmail.com>
2026-06-30 02:03:59 -0600

    regresamos y probamos

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ Developed by www.kuper.mx
 
             <div class="large-12 center columns bgamarillo" id="btnUpdate">
                 <h1>Topónimos</h1>
-                <h2>Museo Regional de Guerrero Prueba de la actualizacion</h2>
+                <h2>Museo Regional de Guerrero</h2>
             </div>
 
         </div>
